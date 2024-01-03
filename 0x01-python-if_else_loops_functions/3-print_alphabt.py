4af7d2ddab0d3143382b3dc2271434d31781bb20
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-03 23:03:31 +0300

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end= '')
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
