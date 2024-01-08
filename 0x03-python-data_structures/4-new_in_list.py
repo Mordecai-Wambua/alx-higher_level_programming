c11013ddc64de22481f36d43513b54eb8b531b02
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-08 12:28:31 +0300

    corrections

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
-    new = my_list.copy
+    new = my_list.copy()
     new[idx] = element
     return new
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-        for y in range(0, len(matrix[x])):
-            if not y == 0:
-                print(" ", end="")
-            print("{}".format(matrix[x][y]),end="")
+        for y in x:
+            print("{:d}".format(y), end="")
         print()
