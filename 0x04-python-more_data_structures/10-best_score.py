c6b125934886b7d9b757b2fe1a8bec8231d4bc81
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-10 17:44:26 +0300

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        x = max(a_dictionary.values())
+        for k, v in a_dictionary.items():
+            if v == x:
+                return
