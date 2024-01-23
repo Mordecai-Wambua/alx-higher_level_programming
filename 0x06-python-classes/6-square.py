7a16370bf862c8a7b61eb55be152be7235386891
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-23 22:10:05 +0300

    corrections

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square:
             size (int): size of square
             position (tuple): position of square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
