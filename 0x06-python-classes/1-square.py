1a47b4f95192a3395cda05036826e8e03166eca1
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-23 20:31:05 +0300

    task 01

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""empty class Square that defines a square"""
+
+
+class Square:
+    """empty class Square that defines a square"""
+    def __init__(self, size):
+        """Instantiation of the square
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
