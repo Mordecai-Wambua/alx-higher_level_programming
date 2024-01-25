9b85c79b73751051a7ffe51183f9d555d458ecb1
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-25 14:18:21 +0300

    task 09

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,6 +9,6 @@ def magic_calculation(a, b):
             else:
                 result += (a ** b) / i
         except Exception as e:
-            result = b + a
+            result += b + a
             break
     return (result)
