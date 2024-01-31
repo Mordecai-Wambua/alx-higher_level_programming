58a49a635d9faae1a17ec1fa777547f2c418f599
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-31 20:25:33 +0300

    correction task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -34,7 +34,8 @@ def matrix_divided(matrix, div):
 
         if not all(len(row) == len(matrix[0]) for row in matrix):
             raise TypeError("Each row of the matrix must have the same size")
+
         return [list(map(lambda x: round(x / div, 2), row)) for row in matrix]
 
     except (TypeError, ZeroDivisionError) as i:
-        return i
+        raise(i)
