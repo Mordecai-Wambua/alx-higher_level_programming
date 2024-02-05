062637d5d5684a475d96f929c6a1c2d29ff394b9
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-02-05 20:12:43 +0300

    task 01 correction

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""class to inherit from list."""
+"""class to inherit class list."""
 
 
 class MyList(list):
-    """Inherit from list."""
+    """Inherit properties of class list."""
 
     def print_sorted(self):
         """Sort the list in ascending order."""
