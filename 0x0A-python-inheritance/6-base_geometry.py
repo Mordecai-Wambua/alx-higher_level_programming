273975b4735183a4dc5f2c07fda27437ad4453dd
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-02-05 17:32:05 +0300

    task 06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creation of an empty class."""
+
+
+class BaseGeometry:
+    """Based on 5-base_geometry."""
+
+    def area(self):
+        """Unimplemented method."""
+        return Exception('area() is not implemented')
