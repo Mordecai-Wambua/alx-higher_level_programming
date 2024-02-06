10bc2bbed9beb6d73315a0d31a21fe9b187a625d
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-02-07 00:08:22 +0300

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Class defining a student."""
+
+
+class Student:
+    """Define a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiate class.
+
+        Args:
+            first_name: student's first name
+            last_name: student's last name
+            age: student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve dictionary representation.
+
+        Args:
+            attrs: list of strings of attributes
+        """
+        if (type(attrs) == list and
+                all(type(x) == str for x in attrs)):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__
