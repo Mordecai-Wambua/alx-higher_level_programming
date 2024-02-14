0fda6183632c96872e2682462bb7923e27d91ff8
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-02-14 23:18:10 +0300

    advanced

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """The first class Base."""
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -73,3 +75,80 @@ class Base:
                 return ([cls.create(**i) for i in dicts])
         except FileNotFoundError:
             return ([])
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serialize the csv.
+        Args:
+            list_objs (list):  list of instances who inherits of Base
+        """
+        if list_objs is None:
+            list_objs = []
+
+        with open("{}.csv".format(cls.__name__), "w", newline='') as f:
+            writer = csv.writer(f)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([obj.id, obj.width, obj.height, obj.x,
+                                    obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes the csv."""
+        csv_file = str(cls.__name__) + ".csv"
+        objs = []
+        try:
+            with open(csv_file, "r") as f:
+                r = csv.reader(f)
+                for row in r:
+                    if cls.__name__ == "Rectangle":
+                        obj = cls(int(row[0]), int(row[1]), int(row[2]),
+                                  int(row[3]), int(row[4]))
+                    elif cls.__name__ == "Square":
+                        obj = cls(int(row[0]), int(row[1]), int(row[2]),
+                                  int(row[3]))
+                    objs.append(obj)
+                return objs
+        except FileNotFoundError:
+            return ([])
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Open a window and draws all the Rectangles and Squares:"""
+        import turtle
+        import time
+        from random import randrange
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
+
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
