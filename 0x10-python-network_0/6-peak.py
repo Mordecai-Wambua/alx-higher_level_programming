a0428cea115806eefa1157ba2822dfdec6f03b54
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-04-30 08:49:38 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,7 @@
 #!/#!/usr/bin/python3
+"""Find a peak in a list of unsorted integers."""
+
+
 def find_peak(list_of_integers):
     """
     Find the peak in a list of integers
@@ -8,7 +11,7 @@ def find_peak(list_of_integers):
         peak: int
     """
 
-    if len(list_of_integers) == 0:
+    if len(list_of_integers) == 0 or list_of_integers == []:
         return None
     elif len(list_of_integers) == 1:
         return list_of_integers[0]
