46c4b522198b4c9afd7af4e2e4e34d7186519ee5
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-04-30 09:11:56 -0700

    Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to a URL and displays only the status code of the response.
-curl -sIX GET "$1" | grep -i '^HTTP/1.1' | awk '{print $2}' 
+curl -s -o /dev/null -I --w "%{http_code}" "$1"
