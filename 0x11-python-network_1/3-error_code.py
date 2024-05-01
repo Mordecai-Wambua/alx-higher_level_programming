69b378497b8c95f583688c7e405b23cfbf4fed50
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-01 18:59:56 +0300

    task 3 trial

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes in a  URL, sends a request and displays response body."""
+
+
+from urllib.request import Request, urlopen
+from sys import argv
+from  urllib.error import HTTPError
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = Request(url)
+
+    try:
+        response = urlopen(req)
+    except HTTPError as e:
+        print('Error code: ',e.code)
