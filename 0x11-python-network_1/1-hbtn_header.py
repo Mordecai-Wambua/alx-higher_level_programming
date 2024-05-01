51b2ae215432e10f651874ecf79749de87aafd13
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-01 08:17:31 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Takes in a URL, sends a request and displays the value of X-Request-ID."""
-from urllib.request import Request, urlopen
+import urllib.request
 import sys
 
 
 url = sys.argv[1]
-req = Request(url)
-with urlopen(req) as response:
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
     html = response.info()
     print(html['X-Request-Id'])
