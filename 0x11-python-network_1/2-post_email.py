646ffa7a86ede7a6067836113eafddecbe6881d0
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-01 18:48:22 +0300

    task 2: doc

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Send a POST request to the passed URL with the email as a parameter."""
+"""
+Send a POST request to the passed URL with the email as a parameter.
+
+Usage: ./2-post_email.py <url> <email>
+"""
 
 
 import sys
 import urllib.request
 import urllib.parse
 
+
 url = sys.argv[1]
 par = {'email': sys.argv[2]}
 data = urllib.parse.urlencode(par)
 data = data.encode('ascii')
+
 req = urllib.request.Request(url, data)
 with urllib.request.urlopen(req) as response:
     the_page = response.read().decode('utf-8')
