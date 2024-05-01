6ddeb6773fd2e725d745c3654943fbed03ea0872
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-01 08:05:11 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the value."""
-
+"""
+Takes in a URL, sends a request to the URL and displays the value
+of the X-Request-Id variable found in the header of the response.
+"""
 
 from urllib.request import Request, urlopen
 import sys
