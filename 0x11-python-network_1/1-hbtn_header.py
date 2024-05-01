be99dca343493cdd9d3e54890bd0331cef505360
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-01 08:21:18 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request and displays the value of X-Request-ID."""
-import urllib.request
+""" Takes in a URL, sends a request and displays
+the value of the X-Request-Id variable.
+"""
+
 import sys
+import urllib.request
 
 
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    html = response.info()
-    print(html['X-Request-Id'])
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
