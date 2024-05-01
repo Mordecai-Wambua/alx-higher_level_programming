9c695a040ef86a3fd7ff892f4d27f18d07073f29
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-01 08:42:46 -0700

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to the passed URL with the email as a parameter
+and displays the body of the response.
+"""
+
+
+import sys
+import urllib.request
+import urllib.parse
+
+url = sys.argv[1]
+par = {'email': sys.argv[2]}
+data = urllib.parse.urlencode(par)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    the_page = response.read().decode('utf-8')
+    print(the_page)
