26617a8181917dfe4c12306b74077675b0ccefe6
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-01 08:46:19 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Sends a POST request to the passed URL with the email as a parameter
-and displays the body of the response.
-"""
+"""Send a POST request to the passed URL with the email as a parameter."""
 
 
 import sys
