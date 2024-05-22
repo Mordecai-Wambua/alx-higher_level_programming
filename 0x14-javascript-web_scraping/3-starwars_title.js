67a3f608a85a32f10ea08195e0d9aa1127343484
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-22 21:10:56 +0300

    tasks 03: correction

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/'
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 
 request(url + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
-  const output = JSON.parse(body)
-  console.log(output.title)
+  const output = JSON.parse(body);
+  console.log(output.title);
 });
